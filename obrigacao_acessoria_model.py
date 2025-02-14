8983387ea1a3663989f20451d77338167e079811
marleypm16 <yarlempm16@gmail.com>
2025-02-14 19:06:15 -0300

    feat: create periodicidade enum

--- obrigacao_acessoria_model.py
+++ obrigacao_acessoria_model.py
@@ -1,13 +1,17 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey, Enum
 from db import Base
 from sqlalchemy.orm import relationship
+from enum import Enum as PyEnum
 
+class PeriodicidadeEnum(PyEnum):
+    MENSAL = "mensal"
+    TRIMESTRAL = "trimestral"
+    ANUAL = "anual"
 
 class ObrigacaoAcessoria(Base):
     __tablename__ = 'obrigacaoacessoria'
-    id = Column(Integer, primary_key=True,index=True)
+    id = Column(Integer, primary_key=True, index=True)
     nome = Column(String)
     periodicidade = Column(String)
-    empresa_id= Column(Integer, ForeignKey('empresa.id',ondelete="CASCADE"))
+    empresa_id = Column(Integer, ForeignKey('empresa.id', ondelete="CASCADE"))
     empresa = relationship("Empresa", back_populates="obrigacoes")
-
