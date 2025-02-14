1c9978a8fa16e85a29faa2d8d74b851247663a23
marleypm16 <yarlempm16@gmail.com>
2025-02-14 14:45:12 -0300

    feat : change package import

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-import db
 from fastapi import FastAPI
 
-import rota_empresa
-import rota_obrigacao_acessoria
+
+import empresa_controller
+import obrigacao_acessoria_controller
 
 app = FastAPI()
 
-app.include_router(rota_empresa.router)
-app.include_router(rota_obrigacao_acessoria.router)
+app.include_router(empresa_controller.router)
+app.include_router(obrigacao_acessoria_controller.router)
 
 
 
