64996fbc50321f083d078408f7c16189fdff83cb
marleypm16 <yarlempm16@gmail.com>
2025-02-14 15:13:28 -0300

    feat : add swagger information

--- main.py
+++ main.py
@@ -4,7 +4,14 @@ from fastapi import FastAPI
 import empresa_controller
 import obrigacao_acessoria_controller
 
-app = FastAPI()
+app = FastAPI(
+    title="Api Desafio Dcifre",
+    description="Api permite gerenciar empresas e obrigações Acessórias",
+    contact={
+        "Nome" : "Marley Pinheiro Martins",
+        "email": "marleypm16@gmail.com"
+    }
+)
 
 app.include_router(empresa_controller.router)
 app.include_router(obrigacao_acessoria_controller.router)
